da2954279140553e4a947072fb92169c29984874
Carlos Botina <carlosalberto.botinacarpio@gmail.com>
2023-01-30 08:38:47 -0500

    Clubs endpoints & functionality working

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -1,27 +1,56 @@
 const Club = require('../models/clubs')
+const asyncWrapper = require('../middleware/async')
+const {CustomAPIError} = require('../errors/custom-error')
 
-const getAllClubs = async (req,res)=>{
-    res.end('getAllClubs')
-}
+const getAllClubs = asyncWrapper(
+    async (req, res) => {
+        const clubs = await Club.find()
+        res.json({clubs})
+    }
+)
 
-const getClub = async (req,res)=> {
-    const id = req.params.id
-    res.json({id})
-}
+const getClub = asyncWrapper(
+    async (req, res) => {
+        const id = req.params.id
+        const club = await Club.find({_id:id})
+        if(club.length ===0){
+            throw (new CustomAPIError(`No club with id ${id}`, 404))
+        }
+        res.status(200).json({club})
+    }
+)
 
-const createClub = async (req,res)=> {
-    const club = await Club.create(req.body)
-    res.status(200).json(club)
-}
+const createClub = asyncWrapper(
+    async (req, res) => {
+        const club = await Club.create(req.body)
+        res.status(200).json(club)
+    }
+)
 
-const updateClub = async (req,res)=> {
-    const id = req.params.id
-    res.json({message: "updated", id})
-}
+const updateClub = asyncWrapper(
+    async (req, res) => {
+        const id = req.params.id
+        const updatedClud = await Club.findOneAndUpdate({_id:id},req.body, {
+            runValidators: true,
+            new: true
+        })
 
-const deleteClub = async (req,res)=> {
-    const id = req.params.id
-    res.json({message: "deleted", id})
-}
+        if(!updatedClud){
+            throw new CustomAPIError(`There's no club with id ${id}`, 404)
+        }
+        res.status(200).json({updatedClud})
+    }
+)
 
-module.exports = {getAllClubs, getClub, createClub, updateClub, deleteClub}
+const deleteClub = asyncWrapper(
+    async (req, res) => {
+        const id = req.params.id
+        const deletedClub = await Club.findOneAndDelete({_id:id})
+        if(!deletedClub){
+            throw new CustomAPIError(`There's no club with id ${id}`, 404)
+        }
+        res.status(200).json({deletedClub})
+    }
+)
+
+module.exports = { getAllClubs, getClub, createClub, updateClub, deleteClub }
